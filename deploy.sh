63ba213e3b943acb4b4c2d59afb90bc0f848bef4
southorange <southorange@users.noreply.github.com>
2016-05-26 17:58:19 -0400

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+git config user.name "yarrharr_pirates"
+git config user.email "octagonpentagonhexagon@gmail.com"
+
+git checkout master
+git add dist/index.js
+git add dist/index.min.js
+git commit -m "Update dist [ci skip], ye matey"
+
+git push "https://${GITHUB_SECRET_TOKEN}@${GH_REF}" master > /dev/null 2>&1
