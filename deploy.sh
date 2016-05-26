82bc024d3130ea90bb661a31db8cc6b815631d06
southorange <southorange@users.noreply.github.com>
2016-05-26 19:12:05 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git config user.email "octagonpentagonhexagon@gmail.com"
 
 git checkout master
 git add '*.*'
-git commit -m "Update dist [ci skip], ye matey"
+git commit -m "Update dist [ci skip], ye matey."
 
 git push "https://${GITHUB_SECRET_TOKEN}@${GH_REF}" master > /dev/null 2>&1
