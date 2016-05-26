f443ff6729a00c23cca4cad2260c73f4e91774a3
southorange <southorange@users.noreply.github.com>
2016-05-26 18:01:03 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ git config user.name "yarrharr_pirates"
 git config user.email "octagonpentagonhexagon@gmail.com"
 
 git checkout master
-git add dist/index.js
-git add dist/index.min.js
+git add index.js
+git add index.min.js
 git commit -m "Update dist [ci skip], ye matey"
 
 git push "https://${GITHUB_SECRET_TOKEN}@${GH_REF}" master > /dev/null 2>&1
