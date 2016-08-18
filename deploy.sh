e6aa4197b69d70778a699a58c57a9cb53d14cdc8
southorange <southorange@users.noreply.github.com>
2016-08-18 14:13:10 -0400

    Revert "Revert 5 southorange patch 1"

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-git config user.name "yarrharr_pirates"
+git config user.name "Travis CI"
 git config user.email "octagonpentagonhexagon@gmail.com"
 
 git checkout master
 git add index.js
 git add index.min.js
-git commit -m "Update dist [ci skip], ye matey"
+
+git commit -m "Update dist [ci skip], ye matey."
 
 git push "https://${GITHUB_SECRET_TOKEN}@${GH_REF}" master > /dev/null 2>&1
